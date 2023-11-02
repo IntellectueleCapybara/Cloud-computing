45ba4dd5b7ae0998f4c2d935d443a1aae2c2305e
intellectuele Capibara <81165007+IntellectueleCapybara@users.noreply.github.com>
2023-11-02 11:55:41 +0100

    docker networking commandos verbeteren

--- weekopdracht_2/Docker_Commands.sh
+++ weekopdracht_2/Docker_Commands.sh
@@ -5,17 +5,30 @@ docker network ls
 # Inspect a Docker network
 docker network inspect bridge
 
-# Create a Docker network
-docker network create mijn__prachtige_netwerk
+# Maak twee Docker-netwerken
+docker network create mijn__prachtige_netwerk1
+docker network create mijn__prachtige_netwerk2
 
-# Connect a container to a Docker network
-docker network connect mijn__prachtige_netwerk my_container
+# Start de eerste busybox-container in het eerste netwerk
+docker run --network=mijn__prachtige_netwerk1 --name=busybox1 -d busybox sleep 3600
 
-# Disconnect a container from a Docker network
-docker network disconnect mijn__prachtige_netwerk my_container
+# Start de tweede busybox-container in het tweede netwerk
+docker run --network=mijn__prachtige_netwerk2 --name=busybox2 -d busybox sleep 3600
 
-# Remove a Docker network
-docker network rm mijn__prachtige_netwerk
+# Controleer de netwerkdetails van de eerste container
+docker inspect busybox1
 
+# Controleer de netwerkdetails van de tweede container
+docker inspect busybox2
 
-### DIT moet nog aangepas worden!!!
+# Probeer vanuit de eerste container de tweede container te pingen (dit zou moeten mislukken)
+docker exec busybox1 ping -c 4 busybox2 || echo "Ping mislukt zoals verwacht"
+
+# Verbind de eerste container met het tweede netwerk
+docker network connect mijn__prachtige_netwerk2 busybox1
+
+# Probeer nu opnieuw vanuit de eerste container de tweede container te pingen (dit zou nu moeten werken)
+docker exec busybox1 ping -c 4 busybox2 || echo "Ping mislukt"
+
+# Verwijder de Docker-netwerken
+docker network rm mijn__prachtige_netwerk1 mijn__prachtige_netwerk2
